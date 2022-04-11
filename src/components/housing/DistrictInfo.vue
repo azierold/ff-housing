8fac9eb12ea7d00ad6e40d96ed932065963bcaef
Alexander Zierold <Alexander.Zierold.gm@gmail.com>
2022-04-11 23:26:02 +0200

    Update TIme Handling

--- src/components/housing/DistrictInfo.vue
+++ src/components/housing/DistrictInfo.vue
@@ -1,21 +1,36 @@
 <template>
-<div>
-  <div>{{ district.name }}</div>
-  <div>{{ district.num_open_plots }}</div>
-  <div>{{ district.oldest_plot_time }}</div>
-  <hr>
-</div>
+  <div>
+    <div>{{ district.name }}</div>
+    <div>Open Plots: {{ district.num_open_plots }}</div>
+    <div>Last Update: {{ formatDate(district.oldest_plot_time * 1000) }}</div>
+    <hr />
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+
+const dateOptions = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "shortOffset"
+};
+const dateFormatter = new Intl.DateTimeFormat("de-DE", dateOptions);
 
 export default defineComponent({
   props: {
     district: { type: Object, required: true },
   },
   setup() {
-    return {};
+    const formatDate = (input) => {
+      return dateFormatter.format(input);
+    };
+
+    return { formatDate };
   },
 });
 </script>
